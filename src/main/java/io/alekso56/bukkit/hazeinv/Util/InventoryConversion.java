ab4c88961ed2f5f496848381851bfda9c91153ef
alekso56 <alekso56@users.noreply.github.com>
2025-01-27 04:08:42 +0100

    Update to 1.21.4

--- src/main/java/io/alekso56/bukkit/hazeinv/Util/InventoryConversion.java
+++ src/main/java/io/alekso56/bukkit/hazeinv/Util/InventoryConversion.java
@@ -1,8 +1,8 @@
 package io.alekso56.bukkit.hazeinv.Util;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_21_R2.CraftServer;
-import org.bukkit.craftbukkit.v1_21_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_21_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_21_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import io.alekso56.bukkit.hazeinv.Models.Circle;
--- src/main/java/io/alekso56/bukkit/hazeinv/Util/InventoryStorage.java
+++ src/main/java/io/alekso56/bukkit/hazeinv/Util/InventoryStorage.java
@@ -14,10 +14,10 @@ import javax.annotation.Nullable;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.craftbukkit.v1_21_R2.CraftRegistry;
-import org.bukkit.craftbukkit.v1_21_R2.CraftServer;
-import org.bukkit.craftbukkit.v1_21_R2.inventory.CraftInventory;
-import org.bukkit.craftbukkit.v1_21_R2.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_21_R3.CraftRegistry;
+import org.bukkit.craftbukkit.v1_21_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_21_R3.inventory.CraftInventory;
+import org.bukkit.craftbukkit.v1_21_R3.inventory.CraftItemStack;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.Inventory;
 
